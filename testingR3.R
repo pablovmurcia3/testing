ba2b1cc798197c72b6b1a4dd74afeabc4f338e73
Pablo Velásquez <pablovmurcia@yahoo.com>
2020-06-02 18:33:31 -0500

    resta

---
+++ testingR3.R
@@ -0,0 +1,10 @@
+print("this files was created within RStudio")
+print("And now it lives on Github")
+matrix(c(2,3,4,3), nrow = 2)
+matrix(c(1,2,3,4), nrow=2)
+a <- matrix(c(2,3,4,3), nrow = 2)
+b <- matrix(c(1,2,3,4), nrow=2)
+a+b
+a%*%b
+
+a-b
